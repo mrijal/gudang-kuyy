c4b692a266326691db1a443cdf9b7dc0799efea4
mrijal <mrijal110@gmail.com>
2025-02-05 21:57:35 +0700

    FIX | Register Open

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login',                    [AuthController::class, 'loginPage'])->name('login');
 Route::post('/login',                   [AuthController::class, 'auth'])->name('login.auth');
-// Route::get('/register',                    [AuthController::class, 'registerPage'])->name('register');
-// Route::post('/register',                   [AuthController::class, 'register'])->name('register.insert');
+Route::get('/register',                    [AuthController::class, 'registerPage'])->name('register');
+Route::post('/register',                   [AuthController::class, 'register'])->name('register.insert');
 Route::get('/logout',                   [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
